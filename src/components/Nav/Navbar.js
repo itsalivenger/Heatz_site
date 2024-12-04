02b0fff2ab3687acf7c6e0164b7fc43b173516ef
itsalivenger <alihoussa16@gmail.com>
2024-12-05 00:36:42 +0100

    e

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -26,7 +26,7 @@ function Navbar({ isAuthenticated, isAdmin, onLogout }) {
     <>
       <nav className={`${styles.navbar}`}>
         <Link to={'/'} className={styles["navbar-logo"]}>
-          <img src={"./public/images/logos/vector.svg"} alt='this is the company logo dedrno2' />
+          <img src={"./images/logos/vector.svg"} alt='this is the company logo' />
         </Link>
 
         <div className={`${styles["linksContainer"]} toggledMenu ${menuIsToggled ? styles.toggledMenu : ''}`}>
