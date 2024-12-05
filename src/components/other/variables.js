a301ee62cffffd8a64478a64f88bc4d0ea39dc5b
itsalivenger <alihoussa16@gmail.com>
2024-12-05 11:57:15 +0100

    chi message

--- src/components/other/variables.js
+++ src/components/other/variables.js
@@ -2,7 +2,7 @@
 const localhostFront = 'http://localhost:3000';
 const localhostBack = 'http://localhost:5000';
 // deploy
-const serverDeploy = 'https://heatz-server.vercel.app/';
+const serverDeploy = 'https://heatz-server.vercel.app';
 const clientDeploy = '';
 
 const domain = localhostFront;
