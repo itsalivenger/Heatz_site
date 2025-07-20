dc56372e465301cf4d8eae31d6f2143389bade8f
itsalivenger <alihoussa16@gmail.com>
2025-07-20 17:17:47 +0100

    feat: update social media icons to use react-icons with monochrome background and shadow

--- src/components/socialMedia/socialMedia.js
+++ src/components/socialMedia/socialMedia.js
@@ -1,21 +1,29 @@
-import LazyMedia from '../lazyMedia/LazyMedia';
+import { FaFacebookF, FaInstagram, FaTiktok, FaLinkedinIn } from 'react-icons/fa';
 import styles from './socialMedia.module.css';
 import { Link } from "react-router-dom";
 
 function SocialMedia() {
     return (
         <div className={styles.iconContainer}>
-            <Link target="_blank" to={'https://facebook.com/61565272588880'} className={`icon ${styles.icon}`}>
-                <LazyMedia preload={true} type={'image'} src={'./images/icons/facebook.png'} alt="social media" />
+            <Link target="_blank" to={'https://facebook.com/61565272588880'} className={`icon ${styles.icon}`}> 
+                <div className={styles.iconBg}>
+                  <FaFacebookF color="#fff" size={22} />
+                </div>
             </Link>
-            <Link target="_blank" to={'https://www.instagram.com/heatz.ma/profilecard/?igsh=MTQweW1rdWozNTlmbg=='} className={`icon ${styles.icon}`}>
-                <LazyMedia preload={true} type={'image'} src={'./images/icons/instagram.png'} alt="social media" />
+            <Link target="_blank" to={'https://www.instagram.com/heatz.ma/profilecard/?igsh=MTQweW1rdWozNTlmbg=='} className={`icon ${styles.icon}`}> 
+                <div className={styles.iconBg}>
+                  <FaInstagram color="#fff" size={22} />
+                </div>
             </Link>
-            <Link target="_blank" to={'https://www.tiktok.com/@heatz.ma?_t=8s3diEAAVlN&_r=1'} className={`icon ${styles.icon}`}>
-                <LazyMedia preload={true} type={'image'} src={'./images/icons/tiktok3.png'} alt="social media" />
+            <Link target="_blank" to={'https://www.tiktok.com/@heatz.ma?_t=8s3diEAAVlN&_r=1'} className={`icon ${styles.icon}`}> 
+                <div className={styles.iconBg}>
+                  <FaTiktok color="#fff" size={22} />
+                </div>
             </Link>
-            <Link target="_blank" to={'https://www.youtube.com/channel/UCQ5zQ7QyQG2O5c4z3Y0zQ7Q'} className={`icon ${styles.icon}`}>
-                <LazyMedia preload={true} type={'image'} src={'./images/icons/linkedin.png'} alt="social media" />
+            <Link target="_blank" to={'https://www.youtube.com/channel/UCQ5zQ7QyQG2O5c4z3Y0zQ7Q'} className={`icon ${styles.icon}`}> 
+                <div className={styles.iconBg}>
+                  <FaLinkedinIn color="#fff" size={22} />
+                </div>
             </Link>
         </div>
     );
